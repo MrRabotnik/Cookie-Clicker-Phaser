c478228e8e239b897840e65cb752832623bb945e
Sargis <terbalyantss@gmail.com>
2024-07-17 00:55:18 +0400

    Init 2

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from "./scenes/HelloWorldScene";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
-}
+    type: Phaser.AUTO,
+    width: w,
+    height: h,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 200 },
+        },
+    },
+    scene: [HelloWorldScene],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,39 +1,42 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
-		super('hello-world')
-	}
-
-	preload()
-    {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+export default class HelloWorldScene extends Phaser.Scene {
+    constructor() {
+        super("hello-world");
     }
 
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+    preload() {
+        this.load.setBaseURL("https://labs.phaser.io");
 
-        const particles = this.add.particles('red')
+        this.load.image("sky", "assets/skies/space3.png");
+        this.load.image("logo", "assets/sprites/phaser3-logo.png");
+        this.load.image("red", "assets/particles/red.png");
+    }
+
+    create() {
+        // Get the game width and height
+        const { width, height } = this.scale;
+
+        // Add the sky image and set it to full width and height
+        const sky = this.add.image(0, 0, "sky").setOrigin(0, 0).setDisplaySize(width, height);
+
+        // Add particles
+        const particles = this.add.particles("red");
 
         const emitter = particles.createEmitter({
             speed: 100,
             scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+            blendMode: "ADD",
+        });
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+        // Add the logo with physics and set properties
+        const logo = this.physics.add.image(width / 2, height / 2, "logo");
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
 
-        emitter.startFollow(logo)
+        // Make particles follow the logo
+        emitter.startFollow(logo);
     }
 }
